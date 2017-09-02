40001088fdc98cee1ee54d3ce3d5146f25049c39
luism <luismcampos32@gmail.com>
2017-09-02 03:41:39 -0500

    add button to retrieve another forecast

--- public/client.js
+++ public/client.js
@@ -21,10 +21,12 @@ $('#weather-form').submit(function(e) {
 
 function getWeather(url) {
   $.getJSON(url, function(data) {
+    $('#forecast-wrapper').empty();
     $('#forecast-wrapper').append('<div id="forecast"></div>');
     $('#forecast').append('<div id="current-temp"><i class="fa fa-sun-o" aria-hidden="true"></i>' + data.main.temp + '\xB0</div>');
     $('#forecast').append('<div id="min-temp">Low: ' + data.main.temp_min + '\xB0</div>');
     $('#forecast').append('<div id="max-temp">High: ' + data.main.temp_max + '\xB0</div>');
+    $('#root').append('<form action="/" id="get-another"><button type="submit" class="pure-button">Get Another</button></form>')
   });
 }
 
